fd2b119df27d3af1252dabd9fc706d07fc6ded63
juru-juliette <kangabejuliette@gmail.com>
2019-12-04 15:25:54 +0200

    editing nav

--- play/static/js/script.js
+++ play/static/js/script.js
@@ -1,15 +1,65 @@
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  
-    for (var i=0;i<4;i++) {
-      next=next.next();
-      if (!next.length) {
-        next=$(this).siblings(':first');
+// Header scroll class
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  } else {
+    $('#header').removeClass('header-scrolled');
+  }
+});
+
+if ($(window).scrollTop() > 100) {
+  $('#header').addClass('header-scrolled');
+}
+
+// Smooth scroll for the menu and links with .scrollto classes
+$('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    if (target.length) {
+      var top_space = 0;
+
+      if ($('#header').length) {
+        top_space = $('#header').outerHeight();
+
+        if (! $('#header').hasClass('header-scrolled')) {
+          top_space = top_space - 20;
+        }
       }
-      next.children(':first-child').clone().appendTo($(this));
+
+      $('html, body').animate({
+        scrollTop: target.offset().top - top_space
+      }, 1500, 'easeInOutExpo');
+
+      if ($(this).parents('.nav-menu').length) {
+        $('.nav-menu .menu-active').removeClass('menu-active');
+        $(this).closest('li').addClass('menu-active');
+      }
+
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+        $('#mobile-body-overly').fadeOut();
+      }
+      return false;
+    }
+  }
+});
+
+// Navigation active state on scroll
+var nav_sections = $('section');
+var main_nav = $('.nav-menu, #mobile-nav');
+var main_nav_height = $('#header').outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  nav_sections.each(function() {
+    var top = $(this).offset().top - main_nav_height,
+        bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      main_nav.find('li').removeClass('menu-active menu-item-active');
+      main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('menu-active menu-item-active');
     }
   });
+});
